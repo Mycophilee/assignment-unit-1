d4f45c639beb3581fc51b8b03946aa02d5af3257
Emma Wood <emma.wood2426@gmail.com>
2024-03-30 23:01:14 -0500

    created hello world assignment

---
+++ assignment/helloWorld.js
@@ -0,0 +1,16 @@
+console.log('Hello World!');
+//variables and data types
+
+// strings - representing text, strings have single quotes around them
+let name = 'Emma'
+// variable name with equals the value 
+let x = 1
+let total = 19.00
+total = total + 9;
+
+console.log('total', total);
+
+
+// Boolean - true or false 
+let inStock = true;
+inStock 
